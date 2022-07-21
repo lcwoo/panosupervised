8fb11244ab37509cf334ee7b63656ea717708de7
Soonmin Hwang <jjang9hsm@gmail.com>
2022-07-21 23:28:16 +0000

    (WIP) Add positional encoding to PanoFeature

--- vidar/arch/networks/layers/panodepth/multicam_fusion.py
+++ vidar/arch/networks/layers/panodepth/multicam_fusion.py
@@ -6,6 +6,53 @@ from torchvision.models.resnet import BasicBlock
 from vidar.arch.networks.layers.panodepth.depth_sweeping import MultiDepthTransform
 
 
+# https://github.com/tatp22/multidim-positional-encoding
+class PositionalEncoding2D(nn.Module):
+    def __init__(self, channels):
+        super(PositionalEncoding2D, self).__init__()
+        self.org_channels = channels
+        channels = int(math.ceil(channels / 4) * 2)
+        self.channels = channels
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2).float() / channels))
+        self.register_buffer("inv_freq", inv_freq)
+        self.cached_penc = None
+
+    def get_emb(self, sin_inp):
+        """
+        Gets a base embedding for one dimension with sin and cos intertwined
+        """
+        emb = torch.stack((sin_inp.sin(), sin_inp.cos()), dim=-1)
+        return torch.flatten(emb, -2, -1)
+
+    def forward(self, tensor):
+        """
+        :param tensor: A 4d tensor of size (batch_size, x, y, ch)
+        :return: Positional Encoding Matrix of size (batch_size, x, y, ch)
+        """
+        if len(tensor.shape) != 4:
+            raise RuntimeError("The input tensor has to be 4d!")
+
+        if self.cached_penc is not None and self.cached_penc.shape == tensor.shape:
+            return self.cached_penc
+
+        self.cached_penc = None
+        batch_size, x, y, orig_ch = tensor.shape
+        pos_x = torch.arange(x, device=tensor.device).type(self.inv_freq.type())
+        pos_y = torch.arange(y, device=tensor.device).type(self.inv_freq.type())
+        sin_inp_x = torch.einsum("i,j->ij", pos_x, self.inv_freq)
+        sin_inp_y = torch.einsum("i,j->ij", pos_y, self.inv_freq)
+        emb_x = self.get_emb(sin_inp_x).unsqueeze(1)
+        emb_y = self.get_emb(sin_inp_y)
+        emb = torch.zeros((x, y, self.channels * 2), device=tensor.device).type(
+            tensor.type()
+        )
+        emb[:, :, : self.channels] = emb_x
+        emb[:, :, self.channels : 2 * self.channels] = emb_y
+
+        self.cached_penc = emb[None, :, :, :orig_ch].repeat(tensor.shape[0], 1, 1, 1)
+        return self.cached_penc
+
+
 class MultiDepthSweepFusion(nn.Module):
     """
     Cylindrical depth sweeping based fusion module using multiple depth hypotheses

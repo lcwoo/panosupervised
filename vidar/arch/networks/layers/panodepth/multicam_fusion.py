a8d86a8bcfb006296ed537323bd33aea36e90ba0
Soonmin Hwang <jjang9hsm@gmail.com>
2022-07-22 08:51:10 +0000

    Clean up

--- vidar/arch/networks/layers/panodepth/multicam_fusion.py
+++ vidar/arch/networks/layers/panodepth/multicam_fusion.py
@@ -1,6 +1,7 @@
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchvision.models.resnet import BasicBlock
 
 from vidar.arch.networks.layers.panodepth.depth_sweeping import MultiDepthTransform
@@ -56,17 +57,20 @@ class PositionalEncoding2D(nn.Module):
 class MultiDepthSweepFusion(nn.Module):
     """
     Cylindrical depth sweeping based fusion module using multiple depth hypotheses
+    Project features for all views (per-camera + per-depth), apply view-attention, then apply a ResNet block
 
     Parameters
     ----------
-    scale_and_shapes: Dict of {String: Tuple(scale, in_shape, out_shape)}
+    scale_and_shapes: Dict of {String: Tuple(in_scale, in_shape, out_scale, out_shape)}
         input and output shapes of this module per camera
+    view_attention: bool
+        turn on/off view attention
+    positional_encoding: int
+        the number of channels for positional encoding
     depth_hypotheses: List of float
         Hypothesized scalar depth values
-    agg_op: str
-        Aggregation method
     """
-    def __init__(self, scale_and_shapes, depth_hypotheses=[3, 5, 10, 30], agg_op='concat'):
+    def __init__(self, scale_and_shapes, view_attention=False, positional_encoding=0, depth_hypotheses=[3, 5, 10, 30]):
         super().__init__()
         self.per_camera_transforms = nn.ModuleDict({
             camera: MultiDepthTransform(camera, *shapes, given_depths=depth_hypotheses)
@@ -74,39 +78,47 @@ class MultiDepthSweepFusion(nn.Module):
 
         # TODO(soonminh): Assume all camera features have the same shape
         _, in_shape, _, out_shape = list(scale_and_shapes.values())[0]
+        num_cameras = len(scale_and_shapes.keys())
+        num_depths = len(depth_hypotheses)
 
-        assert agg_op in ('concat'), 'Unknown aggregation operation: {}'.format(agg_op)
-        self.agg_op = agg_op
+        self.prepare_att = None
+        if view_attention:
+            self.prepare_att = nn.Conv2d(in_shape[0] * num_cameras * num_depths, num_cameras * num_depths, kernel_size=3, padding=1)
+        self.conv = BasicBlock(in_shape[0], out_shape[0], downsample=None)
 
-        if agg_op == 'concat':
-            # Order of aggregation: given depths -> camera (previous)
-            # Order of aggregation: camera -> given depths (WIP)
-            self.conv = BasicBlock(in_shape[0], out_shape[0], downsample=None)
-        elif agg_op == 'self_attention':
-            # TODO(soonminh): Implement attention-based fusion
-            raise NotImplementedError
+        self.get_pos_enc = None
+        if positional_encoding > 0:
+            self.get_pos_enc = PositionalEncoding2D(positional_encoding)
+
 
     def forward(self, feats, meta, return_logs=False):
-        """
-        feats: dictionary of features
-            e.g., feats = {camera: feat for cam, feat in feats.items()}
-        """
-        # 1. Per-camera / Per-depth cylindrical sweeping
+        # 1. Cylinderical sweeping for all views (per-camera + per-depth)
         panofeats = []
         for cam, feat in feats.items():
             panofeat = self.per_camera_transforms[cam](feat, meta)
             panofeats.extend(panofeat)
 
         # 2. Aggregate multicam cylindrical features
-        if self.agg_op == 'concat':
-            num_views = torch.concat([panofeat.detach().sum(axis=1, keepdim=True) != 0.0 for panofeat in panofeats], axis=1)
-            num_views = num_views.sum(axis=1, keepdim=True).clamp(min=1.0)
-            multicam_panofeat = torch.stack(panofeats, axis=1).sum(axis=1) / num_views
+        num_views = torch.concat([panofeat.detach().sum(axis=1, keepdim=True) != 0.0 for panofeat in panofeats], axis=1)
+        num_views = num_views.sum(axis=1, keepdim=True).clamp(min=1.0)
+
+        if self.prepare_att is None:
+            out = torch.stack(panofeats, axis=1).sum(axis=1) / num_views
         else:
-            raise NotImplementedError
+            multicam_panofeat = torch.stack(panofeats, axis=1)
 
-        # 3. TODO(sohwang): cylindrical padding to improve boundaries
+            # 3. View-attention
+            # Get {num_cams x num_depths}-dimensional attention mask and expand it (by broadcasting)
+            # TODO(soonminh): Check if .detach() is required for attn
+            attn = torch.sigmoid(self.prepare_att(torch.cat(panofeats, axis=1))).unsqueeze(2)
+            multicam_panofeat = (multicam_panofeat * attn).sum(axis=1) / num_views
+            out = self.conv(multicam_panofeat)
 
-        out = self.conv(multicam_panofeat)
+        # 3. TODO(soonminh): cylindrical padding to improve boundaries
+
+        # 4. Positional encoding
+        if self.get_pos_enc is not None:
+            pos_enc = self.get_pos_enc(out)
+            out = torch.cat([out, pos_enc], axis=1)
 
         return out
